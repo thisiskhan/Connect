d90ef511efbcd7de50fa23aa6f728f1b5c7b837c
mohammedrazakhan <rk6265766@gmail.com>
2021-01-14 22:02:25 +0530

    Commit

--- lib/Screens/LoginScreen.dart
+++ lib/Screens/LoginScreen.dart
@@ -1,4 +1,6 @@
+import 'package:connect/Screens/HomeScreen.dart';
 import 'package:connect/resources/firebase_repository.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -16,19 +18,50 @@ class _LoginScreenState extends State<LoginScreen> {
   }
 
   Widget loginButton() {
-    return FlatButton(
-      padding: EdgeInsets.all(35),
-      child: Text(
-        "Login", 
-        style: TextStyle(
-          fontSize: 35,
-          fontWeight: FontWeight.w900,
-          letterSpacing: 1.2,
-          
+    return Center(
+      child: FlatButton(
+        padding: EdgeInsets.all(35),
+        child: Text(
+          "Login",
+          style: TextStyle(
+            fontSize: 35,
+            fontWeight: FontWeight.w900,
+            letterSpacing: 1.2,
+          ),
         ),
+        onPressed: () => performLogin(),
       ),
-      onPressed: () => performLogin,
     );
   }
-  void performLogin(){}
+
+   performLogin() {
+    print("tring to perform login");
+    _repository.signIn().then((FirebaseUser user) {
+      print("something");
+      if (user != null) {
+        authenticateUser(user);
+      } else {
+        print("There was an error");
+      }
+    });
+  }
+
+   authenticateUser(FirebaseUser user) {
+    _repository.authenticateUser(user).then((isNewUser) {
+      if (isNewUser) {
+        _repository.addDataToDb(user).then((value) {
+          Navigator.pushReplacement(context,
+              MaterialPageRoute(builder: (context) {
+            return HomeScreen();
+          }));
+        });
+      } else {
+        Navigator.pushReplacement(context,
+            MaterialPageRoute(builder: (context) {
+          return HomeScreen();
+        }));
+      }
+    });
+  }
+
 }
--- lib/resources/firebase_repository.dart
+++ lib/resources/firebase_repository.dart
@@ -1,11 +1,16 @@
 import 'package:connect/resources/firebase_methods.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
+
 class FirebaseRepository {
-  FirebaseMethods _firebaseMetods = FirebaseMethods();
+  FirebaseMethods _firebaseMethods = FirebaseMethods();
 
-  Future<FirebaseUser> getCurrentUser() => _firebaseMetods.getCurrentUser();
+  Future<FirebaseUser> getCurrentUser() => _firebaseMethods.getCurrentUser();
 
+  Future<FirebaseUser> signIn() => _firebaseMethods.signIn();
 
+  Future<bool> authenticateUser(FirebaseUser user) =>
+      _firebaseMethods.authenticateUser(user);
 
+  Future<void> addDataToDb(FirebaseUser user) => _firebaseMethods.addDataToDb(user);
 }
